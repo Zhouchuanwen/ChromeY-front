0987f5c2fda2e054ed3d581d9583d8930eb60fd8
alan <2194999896@qq.com>
2017-01-11 08:58:50 +0800

    init project

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+    // 配置Grunt各种模块的参数
+    grunt.initConfig({
+        connect: {
+            options: {
+                hostname: "127.0.0.1",
+                keepalive: true,
+                open: false
+            },
+            proxies: [{
+                context: '/users',
+                changeOrigin: true,
+                host: "127.0.0.1",
+                port: 8080
+            },
+                {
+                    context: '/essays',
+                    changeOrigin: true,
+                    host: "127.0.0.1",
+                    port: 8080
+                },{
+                    context: '/login',
+                    changeOrigin: true,
+                    host: "127.0.0.1",
+                    port: 8080
+                }],
+            mc: {
+                options: {
+                    port: 8081,
+                    protocol: "http",
+                    middleware: function(connect) {
+                        return [
+                            require('grunt-connect-proxy/lib/utils').proxyRequest,
+                            connect.static('app/')
+                        ];
+                    }
+                }
+            }
+        }
+    });
+
+    // 从node_modules目录加载模块文件
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('mc', ['configureProxies', 'connect:mc']);
+    // 默认启动mc，合并
+    grunt.registerTask('default', ['mc']);
+};
