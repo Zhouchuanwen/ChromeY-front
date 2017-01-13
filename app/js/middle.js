b0499bc9e1ed09409328994cd077a98b5422eb12
alan <2194999896@qq.com>
2017-01-13 14:49:04 +0800

    init project html&css

---
+++ app/js/middle.js
@@ -0,0 +1,10 @@
+
+$(function () {
+    $('#pay').mouseenter(function () {
+        $('#zhifubao').css("display","block");
+    });
+
+    $('#pay').mouseout(function () {
+        $('#zhifubao').css("display","none");
+    });
+});
