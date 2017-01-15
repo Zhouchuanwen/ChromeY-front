e2567aeeb97e716654a1de17032227e1ce165df3
alan <2194999896@qq.com>
2017-01-15 19:52:38 +0800

    完成接口调试

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ####jquery+echarts+html&css:
     之后可以使用响应式元素＋前端框架angular
 
+
+####界面:
+    ![image](https://github.com/Zhouchuanwen/ChromeY-front/blob/master/app/img/details.jpg)
--- app/js/middle.js
+++ app/js/middle.js
@@ -11,10 +11,82 @@ $(function () {
     $('.drop').change(function () {
         var time=$("#time  option:selected").attr("value");
         var type=$("#type  option:selected").attr("value");
-        getHistoryByDate(time);
+        // getHistoryByDate(time);
+        getCount(time);
     });
 
 });
+
+var bin_option = {
+    backgroundColor: '#2c343c',
+
+    title: {
+        text: '饼图',
+        left: 'center',
+        top: 20,
+        textStyle: {
+            color: '#ccc'
+        }
+    },
+
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+
+    visualMap: {
+        show: false,
+        min: 80,
+        max: 600,
+        inRange: {
+            colorLightness: [0, 1]
+        }
+    },
+    series : [
+        {
+            name:'访问来源',
+            type:'pie',
+            radius : '55%',
+            center: ['50%', '50%'],
+            data:[],
+            roseType: 'angle',
+            label: {
+                normal: {
+                    textStyle: {
+                        color: 'rgba(255, 255, 255, 0.3)'
+                    }
+                }
+            },
+            labelLine: {
+                normal: {
+                    lineStyle: {
+                        color: 'rgba(255, 255, 255, 0.3)'
+                    },
+                    smooth: 0.2,
+                    length: 10,
+                    length2: 20
+                }
+            },
+            itemStyle: {
+                normal: {
+                    color: '#c23531',
+                    shadowBlur: 200,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            },
+            animationType: 'scale',
+            animationEasing: 'elasticOut',
+            animationDelay: function (idx) {
+                return Math.random() * 200;
+            }
+        }
+    ]
+};
+
+
+
+
+
 
 var getToday=function () {
     $.ajax({
@@ -72,7 +144,19 @@ var getCount=function (state) {
         data:{state:state},
         dataType:"json",
         success:function (data) {
-            console.log(data);
+            console.log(data.data);
+
+            var temp=data.data.slice(0,5);
+            temp.forEach(function (e) {
+                var obj={
+                    value:e.num,
+                    name:e.title
+                };
+                bin_option.series[0].data.push(obj);
+            });
+            bin_option.series[0].data.sort(function (a, b) { return a.value - b.value});
+            var myChart = echarts.init(document.getElementById('main'));
+            myChart.setOption(bin_option);
         },
         error:function (XMLHttpRequest, textStatus) {
             alert(XMLHttpRequest.status);
@@ -81,3 +165,5 @@ var getCount=function (state) {
         }
     });
 };
+
+
